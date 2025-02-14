f0b4736c166f2002e80fb0eb54e5f71914c376d1
Hardeep025 <hkraj0209@gmail.com>
2025-02-14 14:23:59 -0500

    events page completed

--- lesson-6/js/events.js
+++ lesson-6/js/events.js
@@ -10,7 +10,9 @@ function bgChange() {
     document.body.style.backgroundColor = rndCol;
 }
 // Event Listeners/Handlers
-btn.onclick = bgChange;
+//btn.onclick = bgChange;
+//btn.onmouseover = bgChange;
+
 
 
 /* STEP 1: Experiment with a variety of different events - comment out the above onclick listener and build an alternate one below - try the following - onfocus/onblur, ondblclick, onmouseover/onmouseout, window.onkeypress/onkeydown/onkeyup
@@ -21,11 +23,15 @@ btn.onclick = bgChange;
 // Comment out the above event listener/handler, and add the same event as an attribute directly to the button element
 
 /* STEP 3: Let's apply event handlers/listeners to a group of buttons - comment out the above BUTTON element, and create three new ones, A, B, and C */
-
-
+// const buttons = document.querySelectorAlla("button");
+// console.log(buttons);
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].onclick = bgChange;
+// }
 /* STEP 4a: addEventListener() and removeEventListener()
 Let's rewrite the above code with these two methods - comment out the A, B, C buttons and restore the original button - then comment out the above code */
-
+btn.addEventListener("dbclick", bgChange);
+btn.removeEventListener("onmouseover", bgChange)
 
 /* STEP 4b: We can also put the entire bgChange() function inside an anonymous function, if we'd like: */
 
