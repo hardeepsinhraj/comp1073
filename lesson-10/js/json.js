bbb6f027b2f998af24afa49df437c7999887845f
hardeepsinhraj <hkraj0209@gmail.com>
2025-03-21 15:10:17 -0400

    Done lesson-10

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -63,7 +63,7 @@ function showTopFlavors(jsonObj) {
         article.appendChild(img);
         article.appendChild(ul);
         // STEP 10i: Append each complete ARTICLE element to the SECTION element
-        
+        section.appendChild(article);
    
 };
 // STEP 11: The instructor will edit the JSON file - refresh your page to see the updated content
